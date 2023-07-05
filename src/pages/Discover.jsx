7ccf37732e264c38c52863224fc98d13a1f393af
unknown <minh.trancong@NSK.LOCAL>
2023-07-05 17:37:37 +0900

    init first api (fail)

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,14 @@
 import { Error, Loader, SongCard } from "../components";
 import {genres} from '../assets/constants';
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
-    const gerneTitle = "Pop";
+  const {data, isFetching, error} = useGetTopChartsQuery();
+  const gerneTitle = "Pop";
+
+  if(isFetching) return <Loader title="Loading songs..." />
+
+  if(error) return <Error />
 
     return (
     <div className="flex flex-col">
@@ -14,7 +20,7 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7].map((song, i) => (
+        {data.map((song, i) => (
             <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
