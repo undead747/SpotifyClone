245603d24dc1ce1d4fdfd76bf015ecd0288c189e
TechCare <narutominh747@gmail.com>
2023-08-26 14:38:16 +0900

    init

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '0a0baa13demshd14ad00b42e7f18p1de629jsn40a5ac6f5932')
+
+            return headers
+        }
+     }),
+     endpoints: (builder) => ({
+        getTopCharts: builder.query({query: () => '/charts/world'})
+     })
+})
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
+import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCore';
+
+export const store = configureStore({
+  reducer: {
+    player: playerReducer,
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer
+  },
+
+});
+
+
